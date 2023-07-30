c5f8aee710e90b09e6bf9c3c258028edf396f414
brian <90752841+wokbjso@users.noreply.github.com>
2023-07-30 21:47:53 +0900

    Add:styled-components, react-router-dom, react-responsive, theme 등 초기세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { styled } from "styled-components";
+import { Default, Desktop, Mobile, Tablet } from "./assets/mediaQuery";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<About />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import GlobalStyles from "./styles/GlobalStyle";
+import theme from "./styles/theme";
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
