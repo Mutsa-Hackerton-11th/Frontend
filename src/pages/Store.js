d1cd9292066edca00c6b14a47b411c4f8b4098fa
brian <90752841+wokbjso@users.noreply.github.com>
2023-08-07 15:53:48 +0900

    Fixed:쇼핑몰 likes 삭제

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -9,7 +9,6 @@ export default function Store() {
         "https://cdn.pixabay.com/photo/2015/02/14/02/20/wedding-636021_1280.jpg",
       name: "쇼핑몰 이름",
       introduce: "간단 소개",
-      likes: 3,
       hotOrNew: hotOrNew.HOT,
     },
     {
@@ -17,23 +16,6 @@ export default function Store() {
         "https://cdn.pixabay.com/photo/2023/05/13/14/35/white-flower-7990645_1280.jpg",
       name: "쇼핑몰 이름",
       introduce: "간단 소개",
-      likes: 45,
-      hotOrNew: hotOrNew.HOT,
-    },
-    {
-      image:
-        "https://cdn.pixabay.com/photo/2015/02/14/02/20/wedding-636021_1280.jpg",
-      name: "쇼핑몰 이름",
-      introduce: "간단 소개",
-      likes: 193,
-      hotOrNew: hotOrNew.NEW,
-    },
-    {
-      image:
-        "https://cdn.pixabay.com/photo/2023/05/13/14/35/white-flower-7990645_1280.jpg",
-      name: "쇼핑몰 이름",
-      introduce: "간단 소개",
-      likes: 450,
       hotOrNew: hotOrNew.HOT,
     },
     {
@@ -41,7 +23,6 @@ export default function Store() {
         "https://cdn.pixabay.com/photo/2015/02/14/02/20/wedding-636021_1280.jpg",
       name: "쇼핑몰 이름",
       introduce: "간단 소개",
-      likes: 193,
       hotOrNew: hotOrNew.NEW,
     },
     {
@@ -49,7 +30,20 @@ export default function Store() {
         "https://cdn.pixabay.com/photo/2023/05/13/14/35/white-flower-7990645_1280.jpg",
       name: "쇼핑몰 이름",
       introduce: "간단 소개",
-      likes: 450,
+      hotOrNew: hotOrNew.HOT,
+    },
+    {
+      image:
+        "https://cdn.pixabay.com/photo/2015/02/14/02/20/wedding-636021_1280.jpg",
+      name: "쇼핑몰 이름",
+      introduce: "간단 소개",
+      hotOrNew: hotOrNew.NEW,
+    },
+    {
+      image:
+        "https://cdn.pixabay.com/photo/2023/05/13/14/35/white-flower-7990645_1280.jpg",
+      name: "쇼핑몰 이름",
+      introduce: "간단 소개",
       hotOrNew: hotOrNew.NEW,
     },
     {
@@ -57,7 +51,6 @@ export default function Store() {
         "https://cdn.pixabay.com/photo/2015/02/14/02/20/wedding-636021_1280.jpg",
       name: "쇼핑몰 이름",
       introduce: "간단 소개",
-      likes: 193,
       hotOrNew: hotOrNew.HOT,
     },
     {
@@ -65,7 +58,6 @@ export default function Store() {
         "https://cdn.pixabay.com/photo/2023/05/13/14/35/white-flower-7990645_1280.jpg",
       name: "쇼핑몰 이름",
       introduce: "간단 소개",
-      likes: 450,
       hotOrNew: hotOrNew.NEW,
     },
   ];
